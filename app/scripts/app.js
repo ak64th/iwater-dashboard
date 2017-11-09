0122934d3f09abf9382352945a0d10187bd6c295
郁骏 <ak64th@gmail.com>
2017-11-09 14:33:59 +0800

    feat(map): change visualmap style

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,14 +9,18 @@ mapChart.setOption({
         formatter: (item) => `${item.name}: ${item.value[2]}台`,
     },
     visualMap: {
-        type: 'piecewise',
-        min: 0,
-        max: 200,
-        calculable: true,
-        color: ['#d94e5d', '#eac736', '#50a3ba'],
+        type: 'piecewise', pieces: [
+            {gt: 500, symbol: 'circle', color: '#f6c40a', label: '500以上'},
+            {gt: 350, lte: 500, symbol: 'circle', color: '#ff6800'},
+            {gte: 200, lte: 350, symbol: 'circle', color: '#1857e7'},
+            {lt: 200, symbol: 'circle', color: '#00e498', label: '200以下'},
+        ],
         textStyle: {
             color: '#fff',
         },
+        symbolWidth: 12,
+        symbolHeight: 12,
+        symbolGap: 6,
     },
     geo: {
         map: 'china',
@@ -47,7 +51,7 @@ mapChart.setOption({
             data: [
                 {
                     name: '大连',
-                    value: [121.62, 38.92, 47],
+                    value: [121.62, 38.92, 147, 520],
                 },
             ],
             symbolSize: (val) => val[2] / 5,
@@ -59,11 +63,6 @@ mapChart.setOption({
                 },
                 emphasis: {
                     show: true,
-                },
-            },
-            itemStyle: {
-                normal: {
-                    color: '#ddb926',
                 },
             },
         },
