d125392da930ea9ff4f3838c00d05be429f47aee
郁骏 <ak64th@gmail.com>
2017-11-15 11:52:16 +0800

    feat: add domready and export it to browser global

--- app/scripts/vendor.js
+++ app/scripts/vendor.js
@@ -2,6 +2,9 @@
 require('babel-polyfill');
 require('whatwg-fetch');
 
+// export a dom ready listener to browser
+require('global/window').domready = require('domready');
+
 // bundle echarts with map data and export to global
 require('global/window').echarts = require('echarts');
 require('echarts/map/js/china');
