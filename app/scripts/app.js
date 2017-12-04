ffc5e40e6a797045fc5eeb86421dc1c6bec9b923
郁骏 <ak64th@gmail.com>
2017-12-04 19:07:12 +0800

    feat(ticket-detail): export TicketDetailView

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,11 @@
 import DashboardView from './dashboard';
 import DeviceListView from './deice-list';
 import DeviceDetailView from './deice-detail';
+import TicketDetailView from './ticket-detail';
 
 export {
     DashboardView,
     DeviceListView,
     DeviceDetailView,
+    TicketDetailView,
 };
