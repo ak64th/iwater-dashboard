c03f7b9e5d98f3bcb074ad7ac1e607a903405e8f
郁骏 <ak64th@gmail.com>
2017-12-04 17:59:10 +0800

    feat(device-detail): add temp chart

--- app/scripts/deice-detail.js
+++ app/scripts/deice-detail.js
@@ -1,6 +1,8 @@
 /* global BMap  */
 import {Datatable, defaultConfig} from './datatable';
 
+const echarts = global.echarts;
+
 export default class DeviceDetailView {
     constructor(root) {
         this.datatable = this.initActivationTable(
@@ -8,6 +10,9 @@ export default class DeviceDetailView {
         );
         this.map = this.initMap(
             root.querySelector('.map-container')
+        );
+        this.tempChart = this.initTempChart(
+            root.querySelector('.chart-container')
         );
     }
 
@@ -46,5 +51,168 @@ export default class DeviceDetailView {
         // remove all data rows
         this.datatable.data.splice(0, this.datatable.data.length);
         this.datatable.insert({data: data});
+    }
+
+    initTempChart(el) {
+        const chart = echarts.init(el);
+        chart.setOption({
+            title: [
+                {
+                    textStyle: {
+                        color: '#666',
+                        fontSize: 14,
+                        align: 'center',
+                    },
+                    text: '热水温度',
+                    top: 140,
+                    left: 148,
+                },
+                {
+                    textStyle: {
+                        color: '#666',
+                        fontSize: 14,
+                        align: 'center',
+                    },
+                    text: '冷水温度',
+                    top: 140,
+                    left: 288,
+                },
+            ],
+            series: [
+                {
+                    type: 'pie',
+                    radius: [30, 50],
+                    animation: false,
+                    label: {normal: {show: false}},
+                    name: 'hot',
+                    center: [180, 80],
+                    data: [
+                        {
+                            name: 'empty',
+                            value: 100,
+                            itemStyle: {
+                                normal: {
+                                    color: '#e6e6e6',
+                                },
+                                emphasis: {
+                                    color: '#e6e6e6',
+                                },
+                            },
+                        },
+                    ],
+                },
+                {
+                    type: 'pie',
+                    radius: [30, 50],
+                    animation: false,
+                    label: {normal: {show: false}},
+                    name: 'cold',
+                    center: [320, 80],
+                    data: [
+                        {
+                            name: 'empty',
+                            value: 100,
+                            itemStyle: {
+                                normal: {
+                                    color: '#e6e6e6',
+                                },
+                                emphasis: {
+                                    color: '#e6e6e6',
+                                },
+                            },
+                        },
+                    ],
+                },
+            ],
+        });
+        this.tempChart = chart;
+        return this.tempChart;
+    }
+
+    updateTempChart(hotTemp, coldTemp) {
+        this.tempChart.setOption({
+            series: [
+                {
+                    name: 'hot',
+                    animation: true,
+                    hoverAnimation: false,
+                    data: [
+                        {
+                            name: 'hot',
+                            value: hotTemp,
+                            itemStyle: {
+                                normal: {
+                                    color: '#f7716c',
+                                },
+                                emphasis: {
+                                    color: '#f7716c',
+                                },
+                            },
+                            label: {
+                                normal: {
+                                    show: true,
+                                    position: 'center',
+                                    formatter: '{c}℃',
+                                    fontSize: 20,
+                                    color: '#333',
+                                },
+                            },
+                        },
+                        {
+                            name: 'empty',
+                            value: 100 - hotTemp,
+                            itemStyle: {
+                                normal: {
+                                    color: '#e6e6e6',
+                                },
+                                emphasis: {
+                                    color: '#e6e6e6',
+                                },
+                            },
+                        },
+                    ],
+                },
+                {
+                    name: 'cold',
+                    animation: true,
+                    hoverAnimation: false,
+                    data: [
+                        {
+                            name: 'cold',
+                            value: coldTemp,
+                            itemStyle: {
+                                normal: {
+                                    color: '#009ac1',
+                                },
+                                emphasis: {
+                                    color: '#009ac1',
+                                },
+                            },
+                            label: {
+                                normal: {
+                                    show: true,
+                                    position: 'center',
+                                    formatter: '{c}℃',
+                                    fontSize: 20,
+                                    color: '#333',
+                                },
+                            },
+                        },
+                        {
+                            name: 'empty',
+                            value: 100 - coldTemp,
+                            itemStyle: {
+                                normal: {
+                                    color: '#e6e6e6',
+                                },
+                                emphasis: {
+                                    color: '#e6e6e6',
+                                },
+                            },
+                        },
+                    ],
+                },
+            ],
+        });
     }
 }
