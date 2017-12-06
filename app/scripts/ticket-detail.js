1f58328f487b88e4a9046988af2703b79c6f9c09
郁骏 <ak64th@gmail.com>
2017-12-06 14:37:51 +0800

    refactor(ticket-detail): change datatable to new way

--- app/scripts/ticket-detail.js
+++ app/scripts/ticket-detail.js
@@ -1,5 +1,5 @@
 import Datatable from 'vanilla-datatables';
-import {defaultConfig} from './dt-utils';
+import {defaultConfig, setLoadingMessage, refreshData} from './dt-utils';
 
 export default class TicketDetailView {
     constructor(root) {
@@ -9,28 +9,17 @@ export default class TicketDetailView {
     }
 
     initOperationTable(el) {
-        const headings = [
-            '操作时间',
-            '操作',
-            '状态',
-            '备注',
-        ];
-        this.datatable = new Datatable(el, Object.assign({}, defaultConfig, {
+        this.datatable = new Datatable(el, {
+            ...defaultConfig,
             layout: {
                 top: '',
             },
-            data: {
-                headings: headings,
-                // add an empty line to make setMessage work correctly
-                data: [new Array(headings.length).fill('')],
-            },
-        }));
+        });
+        setLoadingMessage(this.datatable, '等待载入数据...');
         return this.datatable;
     }
 
     updateDeviceListTable(data) {
-        // remove all data rows
-        this.datatable.data.splice(0, this.datatable.data.length);
-        this.datatable.insert({data: data});
+        refreshData(this.datatable, data);
     }
 }
