640d5576cbd42b9b8f953dce199ad797ddfb10da
郁骏 <ak64th@gmail.com>
2017-12-08 14:19:54 +0800

    feat(customer-detail): export CustomerDetailView

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,4 +2,5 @@ export {default as DashboardView} from './dashboard';
 export {default as DeviceListView} from './deice-list';
 export {default as DeviceDetailView} from './deice-detail';
 export {default as TicketDetailView} from './ticket-detail';
+export {default as CustomerDetailView} from './customer-detail';
 export {fetchJson} from './fetch-json';
